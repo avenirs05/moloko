37e01443587fdcd8ce2530d428572bfd6903b8ec
avenirs05 <avenirs05@gmail.com>
2018-02-15 14:43:41 +0200

    last

--- agrochemicals.php
+++ agrochemicals.php
@@ -5,24 +5,27 @@
 		<div class="col col-lg-10 ml-auto mr-auto">
 			<h2 class="text-center headers-h">Биоудобрение Эффлюент "Буренушка-1"</h2>
 			<img class="bio-main" src="imgs/bioudobr.jpg" alt="">
-			<b>Цена: </b> 200 руб.<br>
-			<b>Количество:</b>  1 упаковка<br>
+			<b>Цена (розница): </b> 100 руб.<br>
+			<b>Количество:</b>  1 литр<br>
 			<span><b>Описание:</b> Эффлюэнт "Буренушка-1" это живое, эффективное, 100% органическое биоудобрение, отличающееся высоким содержанием питательных веществ, как макро так и микроэлементов. Не содержит семян сорных растений а так же яиц гельминтов, которые погибают в результате анаэробного сбраживания. Присутствующие в биоудобрении гуминовые кислоты повышают сопротивляемость растений к неблагоприятным условиям внешней среды: засухе, высоким и низким температурам, токсичным веществам. Увеличивает урожайность растений. Не взрывоопасно, не образует вредных и токсичных соединений при внесении в почву. В качестве сырья используется навоз исключительно от собственного племенного стада. Кормовая база для коров по качеству неизменная. В летний период - свободный выпас, в зимний сбалансированный рацион из комбикормов. овощей и сенажа местного заготовления. Необходимость разбавления в 10 раз, улучшает экономичность удобрения. Срок годности продукта в закрытой таре не ограничен ( при соблюдении правил хранения).</span>
-			<div style="padding-left: 0; padding-right: 0;" class="col col-lg-12 ml-auto mr-auto use-bioud-wrap">
+
+
+
+<!-- 			<div style="padding-left: 0; padding-right: 0;" class="col col-lg-12 ml-auto mr-auto use-bioud-wrap">
 				<h2 class="text-center headers-h" style="margin-top: 30px;">Применение и дозировка</h2>
 				<h5 class="text-center headers-h">Удобрение перед применением развести водой в пропорции 1:10</h5>
 				<p>1. Замачивание в растворе семян, клубней, луковиц, черенков различных культур проводят в течение 12-24 ч. при комнатной температуре.</p>
 				<p>2. Корневую подкормку рассады овощных, бахчевых, плодово-ягодных культур, цветов проводят водным раствором эффлюента по 100 мл на растение, после укоренения рассады данных культур - по 500 мл, кустарников и деревьев - по 5 л. </p>
 				<p>3. Bнекормовую подкормку проводят водным раствором эффлюента в период интенсивного роста растения и в начале образования плодов.</p>
-			</div>
-			<div style="padding-left: 0; padding-right: 0;" class="col col-lg-12 ml-auto mr-auto use-bioud-wrap">
+			</div> -->
+<!-- 			<div style="padding-left: 0; padding-right: 0;" class="col col-lg-12 ml-auto mr-auto use-bioud-wrap">
 				<h2 class="text-center headers-h" style="margin-top: 30px;">ПРОИЗВЕДЕНО</h2>
 				<p>Россия, Московская область, Одинцовский район, село Ершово, KФX "Ивлева", тел.: +7 (916)947-81-60, www.молокопарное.рф</p>
 
 				<p>По вопросам покупки эффлюента: Шиянов Алексей Александрович, тел. +7 (916) 080-49-45, shiyanov-aleksei@mail.ru </p>
 
 				<p>Удобрение получено методом анаэробного сбраживания навоза КРС в мезофильном режиме в биогазовом реакторе. Эффлюент относят к неопасным веществам (4-5 классы опасности по Гост 12.1.007). Практически не имеет запаха. Условия хранения от +30 до -30. Беречь от прямых солнечных лучей. Срок хранения неограничен. Дата розлива партии 15.01.2017 г. Объём 5л. Сертификат Соответствия No POCC RU.АГ81.HO1612 на основании Протокола испытаний N©213-314-2-17/БР.</p>
-			</div>
+			</div> -->
 		</div>		
 
 	</div>
@@ -33,9 +36,40 @@
 	<div class="row">
 		<div id="diff-scans-wrap" class="text-center col col-lg-12">
 			<div id="cert-wrap">
+				<img src="imgs/bioudobr/instr.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/instr-2.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/kp.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/kp-pril.png" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/prot-1.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/prot-2.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/prot-3.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/prot-4.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/prot-5.jpg" alt="">
+			</div>
+			<div id="cert-wrap">
+				<img src="imgs/bioudobr/.jpg" alt="">
+			</div>
+
+			<div id="cert-wrap">
 				<img src="imgs/cert.jpg" alt="">
 			</div>
-			<div  id="result-wrap" class="ml-auto mr-auto">
+			<div id="result-wrap" class="ml-auto mr-auto">
 				<img src="imgs/result-analiz.jpg" alt="">
 			</div>
 		</div>
